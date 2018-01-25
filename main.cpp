9fa238ff462b5cf91662c888e66fb42462f35b9c
MemSadePeSade <MemSadePeSade@gmail.com>
2018-01-25 16:55:55 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main()
 {
 	print(char(-1), std::cout);
 	print(short(0), std::cout);
-	print(int(2130706433), std::cout);
-	print(long long int(8875824491850138409), std::cout);
+	print(2130706433, std::cout);
+	print(8875824491850138409, std::cout);
 	print(std::string("1.0.1.0"), std::cout);
 	print(std::vector<int>{0,1,2,3}, std::cout);
 	print(std::list<int>{0,1,2}, std::cout);
