7c301d0a42dd7cfe2d6cd4af75c0c1f7b2f3ad6d
MemSadePeSade <MemSadePeSade@gmail.com>
2018-01-25 16:48:10 +0300

    fix type

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 	print(char(-1), std::cout);
 	print(short(0), std::cout);
 	print(int(2130706433), std::cout);
-	print(long long(8875824491850138409), std::cout);
+	print(long long int(8875824491850138409), std::cout);
 	print(std::string("1.0.1.0"), std::cout);
 	print(std::vector<int>{0,1,2,3}, std::cout);
 	print(std::list<int>{0,1,2}, std::cout);
